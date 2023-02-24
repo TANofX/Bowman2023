2c0dc00ea5febd632b3b9169079366fe8bc69191
ouimettet <95389556+ouimettet@users.noreply.github.com>
2023-02-24 15:11:35 -0500

    Added conveyer

--- src/main/java/frc/robot/subsystems/ConveyerBelt.java
+++ src/main/java/frc/robot/subsystems/ConveyerBelt.java
@@ -10,19 +10,17 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ConveyerBelt extends SubsystemBase {
   private CANSparkMax conveyerBelt;
+  private double runningSpeed = 0;
+  
   /** Creates a new ConveyerBelt. */
   public ConveyerBelt(double conveyerSpeed) {
     conveyerBelt = new CANSparkMax(0,MotorType.kBrushless);
-    double runningSpeed = 0;
-     conveyerSpeed = runningSpeed;
-    conveyerBelt.set(conveyerSpeed);
+
+    runningSpeed = conveyerSpeed;
   }
-;
+
   public void periodic() {
     // This method will be called once per scheduler run
+    conveyerBelt.set(runningSpeed);
   }
-}
-public void Speed() {
-  double slowConveyer = 0;
-  double fastConveyer = 0;
 }
